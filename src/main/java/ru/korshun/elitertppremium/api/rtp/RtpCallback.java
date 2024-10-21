436a9837f0bc3e705052ace0994963dab3de3ec7
IT-CubeDev <korshunkor2000@gmail.com>
2024-10-21 12:15:20 +0300

    Add files via upload

---
+++ src/main/java/ru/korshun/elitertppremium/api/rtp/RtpCallback.java
@@ -0,0 +1,13 @@
+package ru.korshun.elitertppremium.api.rtp;
+
+public enum RtpCallback {
+    WORLD_BLOCKED,
+    WORLD_IN_BLOCKED,
+    DONT_HAVE_PERMISSION,
+    UNKNOWN_ERROR,
+    MIN_PLAYERS,
+    COOLDOWN,
+    NOT_ENOUGH_FOOD,
+    NOT_ENOUGH_HEAL,
+    SUCCESS;
+}
